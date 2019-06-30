edd4c8f659bcc837222965e17a0870db828d3449
Joey Chen <jay.hatelove@gmail.com>
2019-06-30 16:01:20 +0800

    pass test: board api has error

--- CoreLogic/BoardLogic.cs
+++ CoreLogic/BoardLogic.cs
@@ -11,13 +11,14 @@ namespace CoreLogic
     public class BoardLogic : _BaseLogic
     {
         private readonly BoardDa _boardDa;
-        private ApiService ApiService => new ApiService(GetLogger());
 
         public BoardLogic(Operation operation, BoardDa da = null)
             : base(operation)
         {
             _boardDa = da ?? new BoardDa(operation);
         }
+
+        private ApiService ApiService => new ApiService(GetLogger());
 
         public async Task<IsSuccessResult<BoardListDto>> GetBoardList(SearchParamDto search, int pageSize)
         {
@@ -28,7 +29,7 @@ namespace CoreLogic
             };
 
             // Http 呼叫 Service 取得資料
-            var resp = await ApiService.PostApi<BoardQueryDto, BoardQueryResp>(queryDto);
+            var resp = await BoardQueryResp(queryDto);
 
             if (!resp.IsSuccess || resp.Items == null)
                 return new IsSuccessResult<BoardListDto>() {ErrorMessage = "Error", IsSuccess = false};
@@ -55,6 +56,11 @@ namespace CoreLogic
                 IsSuccess = true,
                 ReturnObject = boardListDto
             };
+        }
+
+        protected virtual async Task<BoardQueryResp> BoardQueryResp(BoardQueryDto queryDto)
+        {
+            return await ApiService.PostApi<BoardQueryDto, BoardQueryResp>(queryDto);
         }
     }
 }
--- CoreLogicTests/BoardLogicTests.cs
+++ CoreLogicTests/BoardLogicTests.cs
@@ -1,10 +1,53 @@
-﻿using NUnit.Framework;
+﻿using System.Threading.Tasks;
+using CoreDAL;
+using CoreLogic;
+using CoreLogic.Dto;
+using CoreService.Dto;
+using CoreWebCommon.Dto;
+using ExpectedObjects;
+using NUnit.Framework;
 
 namespace CoreLogicTests
 {
     [TestFixture]
     public class BoardLogicTests
     {
-        
+        [Test]
+        public async Task board_api_has_error()
+        {
+            var boardLogic = new BoardLogicForTest(new Operation());
+            boardLogic.SetBoardQueryResp(new BoardQueryResp()
+            {
+                IsSuccess = false
+            });
+
+            var boardList = await boardLogic.GetBoardList(new SearchParamDto(), 10);
+
+            var expected = new IsSuccessResult<BoardListDto>()
+            {
+                IsSuccess = false,
+                ErrorMessage = "Error"
+            };
+
+            expected.ToExpectedObject().ShouldMatch(boardList);
+        }
+    }
+
+    internal class BoardLogicForTest : BoardLogic
+    {
+        private BoardQueryResp _boardQueryResp;
+
+        public BoardLogicForTest(Operation operation, BoardDa da = null) : base(operation, da)
+        {
+        }
+
+        internal void SetBoardQueryResp(BoardQueryResp boardQueryResp)
+        {
+            _boardQueryResp = boardQueryResp;
+        }
+        protected override Task<BoardQueryResp> BoardQueryResp(BoardQueryDto queryDto)
+        {
+            return Task.FromResult(_boardQueryResp);
+        }
     }
 }
